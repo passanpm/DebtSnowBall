e30f3f83150c5f57bf44d740e6e80dc6015e91b5
Philip Passantino <passanpm@plu.edu>
2017-06-06 22:39:57 -0700

    StudentLoan.cs

---
+++ DebtSnowBall2017/DebtSnowBall2017/StudentLoan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebtSnowBall2017
+{
+    class StudentLoan : Loan
+    {
+        public StudentLoan(double principle, double interest, double totalOwed, int monthsToPay) : base(principle, interest, totalOwed, monthsToPay)
+        {
+        }
+    }
+}
