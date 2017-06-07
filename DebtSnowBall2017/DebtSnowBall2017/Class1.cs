b04fdb39af677c96c569740cad52f8f3ffda4f10
Philip Passantino <passanpm@plu.edu>
2017-06-06 22:37:38 -0700

    Loan.cs

---
+++ DebtSnowBall2017/DebtSnowBall2017/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebtSnowBall2017
+{
+    class Class1
+    {
+    }
+}
---
+++ DebtSnowBall2017/DebtSnowBall2017/Loan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebtSnowBall2017
+{
+    class Loan
+    {
+        protected double principle;
+
+        protected double interest;
+
+        protected double totalOwed;
+
+        protected int monthsToPay;
+
+        public Loan(double principle, double interest, double totalOwed, int monthsToPay)
+        {
+            this.principle = principle;
+
+            this.totalOwed = totalOwed;
+
+            this.monthsToPay = monthsToPay;
+
+            if(interest > 1.0)
+            {
+                interest *= 0.1;
+            }
+
+            this.interest = interest;
+        }
+    }
+}
